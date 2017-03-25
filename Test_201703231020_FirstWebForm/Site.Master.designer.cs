7838ef82b378ad169bfbab48f20e8b79866f56dc
Boris Town <tbziy@hotmail.com>
2017-03-25 19:17:58 +0800

    Add panelClass.caa

--- Test_201703231020_FirstWebForm/Site.Master.designer.cs
+++ Test_201703231020_FirstWebForm/Site.Master.designer.cs
@@ -22,15 +22,6 @@ namespace Test_201703231020_FirstWebForm {
         protected global::System.Web.UI.WebControls.ContentPlaceHolder HeaderContent;
         
         /// <summary>
-        /// asm 控件。
-        /// </summary>
-        /// <remarks>
-        /// 自动生成的字段。
-        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
-        /// </remarks>
-        protected global::System.Web.UI.ScriptManager asm;
-        
-        /// <summary>
         /// panelShadow 控件。
         /// </summary>
         /// <remarks>
