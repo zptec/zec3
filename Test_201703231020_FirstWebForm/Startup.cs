9718262cb4aa705cf033544cbeb1f2d881d79679
Boris Town <tbziy@hotmail.com>
2017-03-26 11:10:11 +0800

    SignalR

---
+++ Test_201703231020_FirstWebForm/Startup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Test_201703231020_FirstWebForm.Startup))]
+
+namespace Test_201703231020_FirstWebForm
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+            // Any connection or hub wire up and configuration should go here
+            app.MapSignalR();
+        }
+    }
+}
